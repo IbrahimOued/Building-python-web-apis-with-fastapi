f0c8eabe5b2f34541820f1a7b2a7f27126dc89ed
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2023-01-01 22:34:31 +0000

    add delete routes

--- todo-app/todo.py
+++ todo-app/todo.py
@@ -4,14 +4,18 @@ from model import Todo, TodoItem
 todo_router = APIRouter()
 
 todo_list = []
+
+
 @todo_router.post("/todo")
 async def add_todo(todo: Todo) -> Todo:
     todo_list.append(todo)
     return {"message": "Todo added successfully"}
 
+
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
+
 
 @todo_router.get("/todo/{todo_id}")
 async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
@@ -24,6 +28,7 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
         "message": "Todo with supplied ID doesn't exist."
     }
 
+
 @todo_router.put("/todo/{todo_id}")
 async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to review")):
     for todo in todo_list:
@@ -35,3 +40,22 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The I
         return {
             "message": "Todo with spplied ID doesn't exist."
         }
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {
+                "message": "Todo deleted successfully."
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }
+
+
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
