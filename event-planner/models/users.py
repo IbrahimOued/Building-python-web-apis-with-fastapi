b1531f3109f8f58eca5dabc0c2fbc10309310bb9
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2023-01-02 22:07:18 +0000

    add mongodb configurations

--- event-planner/models/users.py
+++ event-planner/models/users.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
 from models.events import Event
-
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
-    events: Optional[List[Event]]
+    events: Optional[List[Link[Event]]]
+    class Settings:
+        name = "users"
     class Config:
         schema_extra = {
             "example": {
