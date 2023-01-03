4533742316624158874d6dec38bce02e04661e07
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2023-01-03 00:32:26 +0000

    complete mongodb integration

--- event-planner/main.py
+++ event-planner/main.py
@@ -10,5 +10,4 @@ app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080,
-    reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
--- event-planner/routes/events.py
+++ event-planner/routes/events.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Body, HTTPException, status
-from models.events import Event
+from database.connection import Database
+from beanie import PydanticObjectId
+from models.events import Event, EventUpdate
 from typing import List
+event_database = Database(Event)
 
 event_router = APIRouter(
     tags=["Events"]
@@ -10,6 +13,7 @@ events = []
 
 @event_router.get("/", response_model=List[Event])
 async def retrieve_all_events() -> List[Event]:
+    events = await event_database.get_all()
     return events
 
 @event_router.get("/{id}", response_model=Event)
@@ -38,6 +42,7 @@ async def retrieve_event(id: int) -> Event:
 
 @event_router.post("/new")
 async def create_event(body: Event = Body(...)) -> dict:
+    await event_database.save(body)
     events.append(body)
     return {
         "message": "Event created successfully"
@@ -55,9 +60,24 @@ async def delete_event(id: int) -> dict:
         detail="Event with supplied ID does not exist"
     )
 
-@event_router.delete("/")
-async def delete_all_events() -> dict:
-    events.clear()
+@event_router.delete("/{id}")
+async def delete_event(id: PydanticObjectId) -> dict:
+    event = await event_database.delete(id)
+    if not event:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Event with supplied ID does not exist"
+        )
     return {
-        "message": "Events deleted successfully",
+        "message": "Event deleted successfully."
     }
+    
+@event_router.put("/{id}", response_model=Event)
+async def update_event(id: PydanticObjectId, body: EventUpdate) -> Event:
+    updated_event = await event_database.update(id, body)
+    if not updated_event:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Event with supplied ID does not exist"
+        )
+    return updated_event
