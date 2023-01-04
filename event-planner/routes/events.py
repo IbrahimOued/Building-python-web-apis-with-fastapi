edcbaf9233311336bc2c709900896baa860ad682
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2023-01-04 22:53:03 +0000

    removed duplicate get endpoint in event router

--- event-planner/routes/events.py
+++ event-planner/routes/events.py
@@ -31,15 +31,6 @@ async def retrieve_event(id: PydanticObjectId) -> Event:
 async def retrieve_all_events() -> List[Event]:
     return events
 
-@event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
-    raise HTTPException(
-        status_code=status. HTTP_404_NOT_FOUND,
-        detail="Event with supplied ID does not exist"
-    )
 
 @event_router.post("/new")
 async def create_event(body: Event, user: str = Depends(authenticate)) -> dict:
